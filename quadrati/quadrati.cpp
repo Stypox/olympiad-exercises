9fd2812e97ed72ab74fca32fce346b68f7e5ebda
Stypox <stypox@pm.me>
2018-10-11 15:15:10 +0200

    Task quadrati is now up-to-date with template.

--- quadrati/quadrati.cpp
+++ quadrati/quadrati.cpp
@@ -1,17 +1,54 @@
-#include <string>
-#include <iostream>
+#include <bits/stdc++.h>
 #ifdef __linux__
 #include <sys/stat.h>
 #endif
+#define INLINE inline __attribute__((always_inline))
 
+namespace io {
+	class Input {
+	#ifndef USE_STDIN
+		char* m_data;
+		const char* m_dataBegin;
+	#endif
+	public:
+		INLINE Input();
+		INLINE ~Input();
+		template <typename T> INLINE Input& operator>> (T& data);
+		INLINE Input& operator>> (std::nullptr_t);
+	} in;
 
-#include <math.h>
+	class Output {
+	#ifndef USE_STDOUT
+		std::FILE* m_file;
+	#endif
+	public:
+		INLINE Output();
+		INLINE ~Output();
+		template <typename T> INLINE Output& operator<< (const T& data);
+	} out;
+}
 
 using i = long long;
 using ui = unsigned long long;
 using f = long double;
+using ch = char;
+using str = std::string;
+using io::in;
+using io::out;
+
+constexpr ui squareNumbers(const ui a, const ui b) {
+	return floor(sqrt(b)) - ceil(sqrt(a)) + 1;
+}
+
+int main() {
+	ui a, b;
+	in >> a >> b;
+	out << squareNumbers(a, b);
+}
 
 namespace io {
+	constexpr const char* inputFilename = "input.txt", * outputFilename = "output.txt";
+
 	template<typename T> struct to_format;
 	template<>         struct to_format<signed   char     > { static constexpr const char * format = "%hhd "; };
 	template<>         struct to_format<unsigned char     > { static constexpr const char * format = "%hhu "; };
@@ -30,22 +67,21 @@ namespace io {
 	template<>         struct to_format<      char*       > { static constexpr const char * format = "%s ";   };
 	template<size_t n> struct to_format<      char[n]     > { static constexpr const char * format = "%s ";   };
 
-
 	template<typename T> T read(char*& str);
-	template<> inline __attribute__((always_inline)) signed   char      read<signed   char     >(char*& str) { return std::strtol  (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) unsigned char      read<unsigned char     >(char*& str) { return std::strtoul (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) signed   short     read<signed   short    >(char*& str) { return std::strtol  (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) unsigned short     read<unsigned short    >(char*& str) { return std::strtoul (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) signed   int       read<signed   int      >(char*& str) { return std::strtol  (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) unsigned int       read<unsigned int      >(char*& str) { return std::strtoul (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) signed   long      read<signed   long     >(char*& str) { return std::strtol  (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) unsigned long      read<unsigned long     >(char*& str) { return std::strtoul (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) signed   long long read<signed   long long>(char*& str) { return std::strtoll (str, &str, 0); }
-	template<> inline __attribute__((always_inline)) unsigned long long read<unsigned long long>(char*& str) { return std::strtoull(str, &str, 0); }
-	template<> inline __attribute__((always_inline)) float              read<float             >(char*& str) { return std::strtof  (str, &str   ); }
-	template<> inline __attribute__((always_inline)) double             read<double            >(char*& str) { return std::strtod  (str, &str   ); }
-	template<> inline __attribute__((always_inline)) long double        read<long double       >(char*& str) { return std::strtold (str, &str   ); }
-	template<> inline __attribute__((always_inline)) std::string        read<std::string       >(char*& str) {
+	template<> INLINE signed   char      read<signed   char     >(char*& str) { return std::strtol  (str, &str, 0); }
+	template<> INLINE unsigned char      read<unsigned char     >(char*& str) { return std::strtoul (str, &str, 0); }
+	template<> INLINE signed   short     read<signed   short    >(char*& str) { return std::strtol  (str, &str, 0); }
+	template<> INLINE unsigned short     read<unsigned short    >(char*& str) { return std::strtoul (str, &str, 0); }
+	template<> INLINE signed   int       read<signed   int      >(char*& str) { return std::strtol  (str, &str, 0); }
+	template<> INLINE unsigned int       read<unsigned int      >(char*& str) { return std::strtoul (str, &str, 0); }
+	template<> INLINE signed   long      read<signed   long     >(char*& str) { return std::strtol  (str, &str, 0); }
+	template<> INLINE unsigned long      read<unsigned long     >(char*& str) { return std::strtoul (str, &str, 0); }
+	template<> INLINE signed   long long read<signed   long long>(char*& str) { return std::strtoll (str, &str, 0); }
+	template<> INLINE unsigned long long read<unsigned long long>(char*& str) { return std::strtoull(str, &str, 0); }
+	template<> INLINE float              read<float             >(char*& str) { return std::strtof  (str, &str   ); }
+	template<> INLINE double             read<double            >(char*& str) { return std::strtod  (str, &str   ); }
+	template<> INLINE long double        read<long double       >(char*& str) { return std::strtold (str, &str   ); }
+	template<> INLINE std::string        read<std::string       >(char*& str) {
 		while (isspace(*str)) ++str;
 		if (*str == '\0') return "";
 
@@ -55,7 +91,7 @@ namespace io {
 
 		return std::string{begin, static_cast<unsigned long>(str-begin)};
 	}
-	template<> inline __attribute__((always_inline)) void               read<void              >(char*& str) {
+	template<> INLINE void               read<void              >(char*& str) {
 		while (isspace(*str)) ++str;
 		if (*str == '\0') return;
 
@@ -64,105 +100,89 @@ namespace io {
 	}
 
 
-	class Input {
-	#ifdef USE_STDIN
-	public:
-		inline __attribute__((always_inline)) Input() {
-		#ifndef DEBUG
-			std::cin.sync_with_stdio(false);
-		#endif
-		}
-		template <typename T> inline __attribute__((always_inline)) Input& operator>> (T& data) {
-			std::cin >> data;
-			return *this;
-		}
-		inline __attribute__((always_inline)) Input& operator>> (std::nullptr_t) {
-			std::string s;
-			std::cin >> s;
-			return *this;
-		}
-	#else
-		char* m_data;
-		const char* m_dataBegin;
-	public:
-		inline __attribute__((always_inline)) Input() {
-			constexpr const char* filename = "input.txt";
-
-			std::FILE* file = fopen(filename, "r");
-			#ifdef __linux__
-			struct stat info;
-			fstat(fileno(file), &info);
-			size_t size = info.st_size;
-			#else
-			fseek(file, 0, SEEK_END);
-			size_t size = ftell(file);
-			fseek(file, 0, SEEK_SET);
-			#endif
-
-			m_data = new char[size + 1];
-			m_dataBegin = m_data;
-			m_data[size] = '\0';
-
-			fread(m_data, sizeof(char), size, file);
-			fclose(file);
-		}
-		inline __attribute__((always_inline)) ~Input() {
-		#ifdef DEBUG // leaking memory is not bad, because it reduces time.
-			delete m_dataBegin;
-		#endif
-		}
-		template <typename T> inline __attribute__((always_inline)) Input& operator>> (T& data) {
-			data = read<T>(m_data);
-			return *this;
-		}
-		inline __attribute__((always_inline)) Input& operator>> (std::nullptr_t) {
-			read<void>(m_data);
-			return *this;
+#ifdef USE_STDIN
+	INLINE Input::Input() {
+	#ifndef DEBUG
+		std::cin.sync_with_stdio(false);
+	#endif
+	}
+	INLINE Input::~Input() {}
+	template <typename T> INLINE Input& Input::operator>> (T& data) {
+		std::cin >> data;
+		return *this;
+	}
+	INLINE Input& Input::operator>> (std::nullptr_t) {
+		std::string s;
+		std::cin >> s;
+		return *this;
+	}
+#else
+	INLINE Input::Input() {
+		std::FILE* file = fopen(inputFilename, "r");
+	#ifdef DEBUG
+		if (!file) {
+			perror(("Unable to open file " + std::string{inputFilename}).c_str());
+			exit(1);
 		}
 	#endif
-	} in;
-
-	class Output {
-	#ifdef USE_STDOUT
-	public:
-		inline __attribute__((always_inline)) Output() {}
-		template <typename T> inline __attribute__((always_inline)) Output& operator<< (const T& data) {
-			std::printf(to_format<T>::format, data);
-			return *this;
-		}
+	#ifdef __linux__
+		struct stat info;
+		fstat(fileno(file), &info);
+		size_t size = info.st_size;
 	#else
-		std::FILE* m_file;
-	public:
-		inline __attribute__((always_inline)) Output() : m_file{fopen("output.txt", "w")} {
-		#ifdef DEBUG
-			if (!m_file) {
-				perror("Unable to open file \"output.txt\"");
-				exit(1);
-			}
-		#endif
-		}
-		inline __attribute__((always_inline)) ~Output() { fclose(m_file); }
-		template <typename T> inline __attribute__((always_inline)) Output& operator<< (const T& data) {
-			std::fprintf(m_file, to_format<T>::format, data);
-			return *this;
+		fseek(file, 0, SEEK_END);
+		size_t size = ftell(file);
+		fseek(file, 0, SEEK_SET);
+	#endif
+
+		m_data = new char[size + 1];
+		m_dataBegin = m_data;
+		m_data[size] = '\0';
+
+		fread(m_data, sizeof(char), size, file);
+		fclose(file);
+	}
+	INLINE Input::~Input() {
+	#ifdef DEBUG // leaking memory is not bad, because it reduces time.
+		delete m_dataBegin;
+	#endif
+	}
+	template <typename T> INLINE Input& Input::operator>> (T& data) {
+		data = read<T>(m_data);
+		return *this;
+	}
+	INLINE Input& Input::operator>> (std::nullptr_t) {
+		read<void>(m_data);
+		return *this;
+	}
+#endif
+
+#ifdef USE_STDOUT
+	INLINE Output::Output() {}
+	INLINE Output::~Output() {}
+	template <typename T> INLINE Output& Output::operator<< (const T& data) {
+		std::printf(to_format<T>::format, data);
+		return *this;
+	}
+#else
+	INLINE Output::Output() : m_file{fopen(outputFilename, "w")} {
+	#ifdef DEBUG
+		if (!m_file) {
+			perror(("Unable to open file " + std::string{outputFilename}).c_str());
+			exit(1);
 		}
 	#endif
-	} out;
-
-	template <> inline __attribute__((always_inline)) Output& Output::operator<< (const std::string& data) {
+	}
+	INLINE Output::~Output() {
+		fclose(m_file);
+	}
+	template <typename T> INLINE Output& Output::operator<< (const T& data) {
+		std::fprintf(m_file, to_format<T>::format, data);
+		return *this;
+	}
+#endif
+	template <> INLINE Output& Output::operator<< (const std::string& data) {
 		this->operator<<(data.c_str());
 		return *this;
 	}
-}
-using io::in;
-using io::out;
-
-constexpr ui squareNumbers(const ui a, const ui b) {
-	return floor(sqrt(b)) - ceil(sqrt(a)) + 1;
-}
-
-int main() {
-	ui a, b;
-	in >> a >> b;
-	out << squareNumbers(a, b);
 }
