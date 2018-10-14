fcd5efda66316a7d80002b6d9c87becb3c223b63
Stypox <stypox@pm.me>
2018-10-14 18:14:25 +0200

    Improved complexity from O(n^2) to O(n) in studioamico task.

--- tasks/studioamico/main.cpp
+++ tasks/studioamico/main.cpp
@@ -3,11 +3,24 @@
 #include <bits/stdc++.h>
 #pragma endregion
 
+int_fast8_t* countingSorted(const int* const marks, const int N) {
+	auto result = new int_fast8_t[N];
+	int counted[11] {0};
+
+	for (int n = 0; n < N; ++n)
+		++counted[marks[n]];
+	
+	for (int mark = 0, n = 0; mark <= 10; ++mark)
+		for (const int countMax = n + counted[mark]; n < countMax; ++n)
+			result[n] = mark;
+
+	return result;
+}
+
 bool associabili(const int N, int* const marks2, int* const marks5) {
-	std::sort(marks2, marks2 + N);
-	std::sort(marks5, marks5 + N);
+	int_fast8_t* sorted2 = countingSorted(marks2, N), * sorted5 = countingSorted(marks5, N);
 	for (int n = 0; n < N; ++n) {
-		if (marks2[n] >= marks5[n])
+		if (sorted2[n] >= sorted5[n])
 			return false;
 	}
 	return true;
