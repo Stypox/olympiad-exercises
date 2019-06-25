670e619c704528b18237b060f29d977a31f1f43f
Stypox <stypox@pm.me>
2019-06-25 09:21:19 +0200

    Add template for UVa Online Judge

---
+++ template-uva/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using si = long long;
+using ui = unsigned long long;
+using flt = long double;
+using ch = char;
+using str = string;
+template<class T>
+using vec = vector<T>;
+
+ifstream in{"input.txt"};
+ofstream out{"output.txt"};
+
+int main() {
+	si T;
+	in >> T;
+
+	for(int t = 0; t != T; ++t) {
+		
+	}
+}
