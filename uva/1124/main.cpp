d638166f5941c3645a59c927dda4d11d32e1b620
Stypox <stypox@pm.me>
2019-06-28 13:55:56 +0200

    Add uva task 1124: AC

---
+++ uva/1124/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using si = long long;
+using ui = unsigned long long;
+using flt = long double;
+using ch = char;
+using str = string;
+template<class T>
+using vec = vector<T>;
+
+#ifdef ONLINE_JUDGE
+istream& in = std::cin;
+ostream& out = std::cout;
+#else
+ifstream in{"input.txt"};
+ofstream out{"output.txt"};
+#endif
+
+int main() {
+	str s;
+	while(getline(in, s)){
+		out<<s<<"\n";
+	}
+}
