09301112556c7e3fbbc209fea16ea0453634f280
Stypox <stypox@pm.me>
2020-07-12 14:34:01 +0200

    Improve uva template

--- src/template-uva/main.cpp
+++ src/template-uva/main.cpp
@@ -1,28 +1,24 @@
+#pragma GCC optimize ("O3")
 #include <bits/stdc++.h>
 using namespace std;
 
-using si = long long;
-using ui = unsigned long long;
-using flt = long double;
-using ch = char;
-using str = string;
-template<class T>
-using vec = vector<T>;
+#define int int64_t
+#define float long double
 
 #ifdef ONLINE_JUDGE
 istream& in = std::cin;
 ostream& out = std::cout;
 template<class... Ts> constexpr void deb(const Ts&...) {}
-template<class T, class P=str, class S=str> constexpr void debc(const T&, P="", S="") {}
+template<class T, class P=string, class S=string> constexpr void debc(const T&, P="", S="") {}
 #else
 ifstream in{"input.txt"};
 ofstream out{"output.txt"};
 void deb() {cout<<"\n";} template<class T, class... Ts> void deb(T t, Ts... args) {cout<<t<<" ";deb(args...);}
-template<class T, class P=str, class S=str> void debc(const T& t, P pre="", S sep=" ") {cout<<pre;for(auto&& e:t)cout<<e<<sep;cout<<"\n";}
+template<class T, class P=string, class S=string> void debc(const T& t, P pre="", S sep=" ") {cout<<pre;for(auto&& e:t)cout<<e<<sep;cout<<"\n";}
 #endif
 
-int main() {
-	si T;
+int32_t main() {
+	int T;
 	in >> T;
 
 	for(int t = 0; t != T; ++t) {
