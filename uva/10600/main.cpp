56c0f2a13d3f9a8d27001518a132efe26f25249e
Stypox <stypox@pm.me>
2020-07-24 12:02:29 +0200

    Improve uva task solution for 10600 for stack overflow
    https://stackoverflow.com/a/63059423/9481500

--- uva/10600/main.cpp
+++ uva/10600/main.cpp
@@ -2,107 +2,114 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int int64_t
-#define float long double
-
-#ifdef ONLINE_JUDGE
-istream& in = std::cin;
-ostream& out = std::cout;
-template<class... Ts> constexpr void deb(const Ts&...) {}
-template<class T, class P=string, class S=string> constexpr void debc(const T&, P="", S="") {}
-#else
-ifstream in{"input.txt"};
-ofstream out{"output.txt"};
-void deb() {cout<<"\n";} template<class T, class... Ts> void deb(T t, Ts... args) {cout<<t<<" ";deb(args...);}
-template<class T, class P=string, class S=string> void debc(const T& t, P pre="", S sep=" ") {cout<<pre;for(auto&& e:t)cout<<e<<sep;cout<<"\n";}
-#endif
-
 struct Node{
-	int parent=-1,depth=0;
-	vector<pair<int,int>> mstConn;
-	vector<int> maxWeightEdgeTo;
+	int parent=-1,depth=0; // needed for union-find disjoint sets and for minimum spanning tree
+	vector<pair<int,int>> mstNeighbors; // [i] -> edge to mstConn[i].first of weight mstConn[i].second
+	vector<int> maxWeightEdgeTo; // [i] -> maximum weight of mst edges to reach nodes[i]
 };
 
 struct Edge{
-	int a,b,w;
-	bool usedInMst=false;
+	int a,b,w; // edge (a,b) of weight w
+	bool usedInMst=false; // to prevent substituting an edge with itself
 	bool operator<(const Edge& other) {
-		return w<other.w;
+		return w<other.w; // sort edges by non-decreasing weight
 	}
 };
 
 int32_t main() {
 	int T;
-	in >> T;
+	cin >> T;
 
 	for(int t = 0; t != T; ++t) {
 		int N,M;
-		in>>N>>M;
+		cin>>N>>M;
 
 		vector<Node> nodes(N);
 		vector<Edge> edges(M);
 		for(int m = 0; m != M; ++m) {
-			in>>edges[m].a>>edges[m].b>>edges[m].w;
+			cin>>edges[m].a>>edges[m].b>>edges[m].w;
+			// transform from 1-indexed to 0-indexed
 			--edges[m].a;--edges[m].b;
 		}
-		sort(edges.begin(), edges.end());
 
+
+		// Union-find disjoint sets functions
+		// i: index of the node to find the root of
 		function<int(int)> findRoot = [&](int i){
 			if (nodes[i].parent==-1)return i;
 			return findRoot(nodes[i].parent);
 		};
+
+		// edge: the edge containing the two vertices to connect
 		function<bool(Edge&)> mergeTrees = [&](Edge& edge){
 			int ra=findRoot(edge.a);
 			int rb=findRoot(edge.b);
 
 			if (ra==rb){
-				// cycle
+				// cycle detected, this edge cannot be used
 				return false;
 			} else {
 				if (nodes[ra].depth>nodes[rb].depth) {
+					// ensure node with smaller depth gets that with bigger depth as parent
 					swap(ra,rb);
 				}
 
 				nodes[ra].parent=rb;
 				nodes[rb].depth=max(nodes[rb].depth,nodes[ra].depth+1);
-				nodes[ra].mstConn.push_back({rb,edge.w});
-				nodes[rb].mstConn.push_back({ra,edge.w});
-				edge.usedInMst=true;
+				nodes[ra].mstNeighbors.push_back({rb,edge.w});
+				nodes[rb].mstNeighbors.push_back({ra,edge.w});
 				return true;
 			}
 		};
 
+		// sort edges to find minimum spanning tree greedily -> O(E log E) = O(V² log V)
+		sort(edges.begin(), edges.end());
+
+		// find cost of minimum spanning tree using Kruskal's -> O(E) = O(V²)
+		// for every node in the mst store its tree neighbors (i.e. the parent and all children)
 		int cost1=0;
 		for(auto&& edge : edges) {
 			if(mergeTrees(edge)){
+				// this edge was just connected, so count it in the mst
+				edge.usedInMst=true;
 				cost1+=edge.w;
 			}
 		}
 
 
+		// tree traversal from a single source to every other vertex in the minimum spanning tree -> O(V²)
+		// i: node index  before: calling node index  start: search root node index  maxWeight: max edge weight so far
 		function<void(int,int,int,int)> populateMaxWeightEdgeTo = [&](int i,int before,int start,int maxWeight) {
 			nodes[start].maxWeightEdgeTo[i]=maxWeight;
-			for(auto&& con : nodes[i].mstConn) {
+			for(auto&& con : nodes[i].mstNeighbors) {
 				if (con.first != before) {
-					populateMaxWeightEdgeTo(con.first,i,start,max(maxWeight,con.second));
+					// go to (one of) the next connected vertex(es)
+					populateMaxWeightEdgeTo(con.first, i, start, max(maxWeight,con.second));
 				}
 			}
 		};
 
 		for(int n = 0; n != N; ++n) {
+			// traverse entire tree starting from node n and store the maximum weight
+			// of the edges connecting node n to all other nodes
 			nodes[n].maxWeightEdgeTo.resize(N);
 			populateMaxWeightEdgeTo(n,-1,n,0);
 		}
 
 
+		// finding cost of second minimum spanning tree -> O(E) = O(V²)
 		int cost2=numeric_limits<int>::max();
 		for(auto&& edge : edges) {
 			if (!edge.usedInMst) {
+				// for every edge not belonging to the mst, try disconnecting the two
+				// vertices it connects by removing the mst edge with the maximum weight
+				// in between the two vertices, and then reconnecting them with the
+				// currently considered vertex. Store the minimum result.
 				int cost = cost1 + edge.w - nodes[edge.a].maxWeightEdgeTo[edge.b];
 				cost2 = min(cost2, cost);
 			}
 		}
 
-		out<<cost1<<" "<<cost2<<"\n";
+		cout<<cost1<<" "<<cost2<<"\n";
 	}
 }
