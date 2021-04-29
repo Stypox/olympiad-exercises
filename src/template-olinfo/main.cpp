9aedd24a169d595e0d8d5edf2100a29db7262717
Stypox <stypox@pm.me>
2021-04-29 16:26:29 +0200

    Improve olinfo template:
    - use signed for main() return type
    - add operator<< overloads for vector and pair

--- src/template-olinfo/main.cpp
+++ src/template-olinfo/main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 ifstream in{"input.txt"};
 ofstream out{"output.txt"};
 #ifdef DEBUG
+template<class A, class B> ostream& operator<<(ostream& o, const pair<A, B>& p) {cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v) {cout<<"[";for(size_t i=0;i<v.size();++i){if(i!=0){cout<<", ";}cout<<v[i];}cout<<"]";return o;}
 void deb() {cout<<"\n";} template<class T, class... Ts> void deb(T t, Ts... args) {cout<<t<<" ";deb(args...);}
 template<class T, class P=string, class S=string> void debc(const T& t, P pre="", S sep=" ") {cout<<pre;for(auto&& e:t)cout<<e<<sep;cout<<"\n";}
 #else
@@ -15,6 +17,6 @@ template<class... Ts> constexpr void deb(const Ts&...) {}
 template<class T, class P=string, class S=string> constexpr void debc(const T&, P="", S="") {}
 #endif
 
-int32_t main() {
+signed main() {
 	
 }
