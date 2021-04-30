62641a98f7bcfcde3d4e8ba445585b0baa7a3383
Stypox <stypox@pm.me>
2021-04-30 14:59:53 +0200

    Improve deb() function in olinfo templates

--- src/template-olinfo/main.cpp
+++ src/template-olinfo/main.cpp
@@ -9,10 +9,10 @@ ifstream in{"input.txt"};
 ofstream out{"output.txt"};
 #ifdef DEBUG
 template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
-template<typename T>struct is_iterable{template<class E>static int16_t f(...);template<class E>static int8_t f(int8_t,typename E::const_iterator=E().end());enum{value=sizeof(f<T>(0))==sizeof(int8_t)};};template<class T,typename enable_if<is_iterable<T>::value,bool>::type=false>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
-void deb(){cout<<"\n";}template<class T,class... Ts>void deb(T t,Ts... args) {cout<<t<<"  ";deb(args...);}
+template<class T,class=decltype(*begin(declval<T>()))>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
 #else
-template<class...Ts>constexpr void deb(const Ts&...){}
+#define deb(...)
 #endif
 
 signed main() {
