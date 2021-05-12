299359966fa5373eae0e51f5133ebd3e1134aca0
Stypox <stypox@pm.me>
2021-05-13 00:02:05 +0200

    Fix olinfo task bilancio: 100/100
    segment tree greedy

--- olinfo/bilancio/main.cpp
+++ olinfo/bilancio/main.cpp
@@ -2,113 +2,87 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using si = long long;
-using ui = unsigned long long;
-using flt = long double;
-using ch = char;
-using str = string;
-template <typename T> using vec = vector<T>;
+#define int int64_t
+#define float long double
 
 ifstream in{"input.txt"};
 ofstream out{"output.txt"};
 #ifdef DEBUG
-void deb() {cout<<"\n";} template<class T, class... Ts> void deb(T t, Ts... args) {cout<<t<<" ";deb(args...);}
-template<class T, class P=str, class S=str> void debc(const T& t, P pre="", S sep=" ") {cout<<pre;for(auto&& e:t)cout<<e<<sep;cout<<"\n";}
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
 #else
-template<class... Ts> constexpr void deb(const Ts&...) {}
-template<class T, class P=str, class S=str> constexpr void debc(const T&, P="", S="") {}
+#define deb(...)
 #endif
 
-si higherPow2(si v) {
-	v--;
-	v |= v >> 1;
-	v |= v >> 2;
-	v |= v >> 4;
-	v |= v >> 8;
-	v |= v >> 16;
-	v++;
-	return v;
-}
+struct SegmentTree {
+	vector<int> dat, idx;
+	SegmentTree(int N) {
+		int s=1;
+		while(s<N) s*=2;
+		s*=2;
+		dat.resize(s);
+		idx.resize(s);
+		iota(idx.begin() + s/2, idx.end(), 0);
+	}
 
-struct ST {
-	vec<si> vals;
-	vec<si> data;
+	void update(int x, int v, int a, int b, int i) {
+		if (x<a || x>=b) return;
+		if (b-a == 1) {
+			dat[i] = v;
+			return;
+		}
 
-	void build(si i, si L, si R) {
-		if (L==(R-1)) {
-			data[i]=L;
+		update(x, v, a, (a+b)/2, i*2);
+		update(x, v, (a+b)/2, b, i*2+1);
+
+		if (dat[i*2] <= dat[i*2+1]) {
+			dat[i]=dat[i*2];
+			idx[i]=idx[i*2];
 		} else {
-			build(i*2,   L,       (L+R)/2);
-			build(i*2+1, (L+R)/2, R      );
-
-			if (vals[data[i*2]] < vals[data[i*2+1]]) {
-				data[i] = data[i*2];
-			} else {
-				data[i] = data[i*2+1];
-			}
+			dat[i]=dat[i*2+1];
+			idx[i]=idx[i*2+1];
 		}
 	}
 
-	ST(const vec<si>& init) {
-		vals = vector<si>(init);
-		vals.resize(higherPow2(init.size()), numeric_limits<si>::max());
-
-		data = vector<si>(2*vals.size());
-		build(1, 0, vals.size());
-	}
-
-	si query(si i, si L, si R, si a, si b) {
-		if (L>=b || R<=a) return -1;
-		if (L>=a && R<=b) return data[i];
-
-		si m1 = query(i*2,   L,       (L+R)/2, a, b);
-		si m2 = query(i*2+1, (L+R)/2, R,       a, b);
-
-		if (m1 == -1) return m2;
-		if (m2 == -1) return m1;
-
-		if (vals[m1] < vals[m2]) {
-			return m1;
-		} else {
-			return m2;
-		}
-	}
-
-	si query(si a, si b) {
-		return query(1, 0, vals.size(), a, b);
+	pair<int, int> query(int x, int y, int a, int b, int i) {
+		deb(x,y,a,b,i);
+		if (a>=y || b<=x) return {numeric_limits<int>::max(), x};
+		if (a>=x && b<=y) return {dat[i], idx[i]};
+		return min(query(x, y, a, (a+b)/2, i*2),
+					  query(x, y, (a+b)/2, b, i*2+1));
 	}
 };
 
-void erase(si N, si K, const vec<si>& U) {
-	ST st(U);
+signed main() {
+	int N,K;
+	in>>N>>K;
 
-	si u = 0;
+	vector<int> U(N);
+	SegmentTree st(N);
+	deb(st.dat, st.idx);
+	for (int n = 0; n < N; ++n) {
+		in >> U[n];
+		st.update(n, U[n], 0, st.dat.size()/2, 1);
+	}
+	deb(st.dat, st.idx);
+
+	int u = 0;
 	while (1) {
 		if (K == N)
 			break;
 		else if (K == 0) {
-			for(si n = 0; n != N; ++n)
+			for(int n = 0; n != N; ++n)
 				out<<U[u+n]<<" ";
 			break;
 		}
-		si minIdx = st.query(u, u+K+1);
+		int minIdx = st.query(u, u + K + 1, 0, st.dat.size()/2, 1).second;
+		deb(u, u+K+1, " ",  minIdx, distance(U.begin(), min_element(U.begin()+u, U.begin()+u+K+1)));
 		out<<U[minIdx]<<" ";
 
-		si diff = minIdx - u;
+		int diff = minIdx - u;
 		K -= diff;
 		N -= diff + 1;
 		u += diff + 1;
 	}
-}
-
-int main() {
-	si N,K;
-	in>>N>>K;
-
-	vec<si> U(N);
-	for (si n = 0; n < N; ++n) {
-		in >> U[n];
-	}
-	
-	erase(N, K, U);
 }
