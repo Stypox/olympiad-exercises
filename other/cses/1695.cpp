ef8682e431f3489efbd55f866afda3d2270322ea
Stypox <stypox@pm.me>
2021-05-24 11:13:15 +0200

    Add cses task 1695: AC
    max maximum flow Ford Fulkerson Edmonds Karp source sink flow network min cut s t

---
+++ other/cses/1695.cpp
@@ -0,0 +1,94 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+istream& in = cin;
+ostream& out = cout;
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+
+signed main() {
+   int N,M;
+   in>>N>>M;
+   int S=0, T=N-1;
+
+   vector<vector<int>> adj(N), capacity(N, vector<int>(N));
+   for(int m=0;m<M;++m){
+      int A,B;
+      in>>A>>B;
+      A--; B--;
+      adj[A].push_back(B);
+      adj[B].push_back(A);
+      capacity[A][B] += 1;
+      capacity[B][A] += 1;
+   }
+
+   vector<int> parent(N);
+   auto bfsAugmentingPath = [&]() -> int {
+      fill(parent.begin(), parent.end(), -1);
+      parent[S] = -2;
+      queue<pair<int,int>> q;
+      q.push({S, numeric_limits<int>::max()});
+
+      while(!q.empty()) {
+         auto [i, flow] = q.front();
+         q.pop();
+
+         for(int e : adj[i]) {
+            if (parent[e] == -1 && capacity[i][e] > 0) {
+               parent[e] = i;
+               int partialFlow = min(flow, capacity[i][e]);
+               if (e==T) return partialFlow;
+               q.push({e, partialFlow});
+            }
+         }
+      }
+      return 0;
+   };
+
+   int flow=0;
+   while(1){
+      int partialFlow=bfsAugmentingPath();
+      if (partialFlow == 0) break;
+      flow+=partialFlow;
+
+      int curr=T;
+      while(curr!=S){
+         capacity[parent[curr]][curr] -= partialFlow;
+         capacity[curr][parent[curr]] += partialFlow;
+         curr=parent[curr];
+      }
+   }
+
+   vector<bool> color(N);
+   color[S]=true;
+   queue<int> q;
+   q.push(S);
+   while(!q.empty()){
+      int i = q.front();
+      q.pop();
+      for(int e:adj[i]){
+         if (capacity[i][e] > 0 && color[e] == false) {
+            color[e] = true;
+            q.push(e);
+         }
+      }
+   }
+
+   out<<flow<<"\n";
+   for(int n=0;n<N;++n){
+      for(int e:adj[n]) {
+         if (color[n] == true && color[e] == false) {
+            out<<n+1<<" "<<e+1<<"\n";
+         }
+      }
+   }
+}
