da78119adba2e86e796b73b6a02b422743e9f08f
Stypox <stypox@pm.me>
2021-11-14 10:42:15 +0100

    Add Kattis task cold: AC

---
+++ more/kattis/cold.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N;
+	cin>>N;
+
+	int res=0;
+	for (int n=0;n<N;++n){
+		int l;
+		cin>>l;
+		res+=(l<0);
+	}
+
+	cout<<res<<"\n";
+}
