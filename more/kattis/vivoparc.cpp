2fda98bea5d2bcfc61f627f50aa5bd267f0a8fae
Stypox <stypox@pm.me>
2022-02-24 16:37:55 +0100

    Implement kattis task vivoparc: AC
    For some reason the code was missing

--- more/kattis/vivoparc.cpp
+++ more/kattis/vivoparc.cpp
@@ -2,19 +2,47 @@
 using namespace std;
 
 struct Node{
-
+	int animal = 0;
+	set<int> sees;
 };
 
 int main(){
 	int N;
 	cin>>N;
 
-	int res=0;
-	for (int n=0;n<N;++n){
-		int l;
-		cin>>l;
-		res+=(l<0);
+	vector<Node> nodes(N);
+	while(1) {
+		int a, b;
+		char t;
+		cin>>a>>t>>b;
+		a--; b--;
+		if(cin.eof()) break;
+
+		nodes[a].sees.insert(b);
+		nodes[b].sees.insert(a);
 	}
 
-	cout<<res<<"\n";
+	function<void(int)> rec = [&](int i) {
+		if (i == N) {
+			for(int n=0;n<N;++n){
+				cout<<n+1<<" "<<nodes[n].animal<<"\n";
+			}
+			exit(0);
+		}
+
+		bool used[5] {false, false, false, false, false};
+		for (int j : nodes[i].sees) {
+			if (j >= i) break;
+			used[nodes[j].animal] = true;
+		}
+
+		for (int a = 1; a <= 4; ++a) {
+			if (!used[a]) {
+				nodes[i].animal = a;
+				rec(i+1);
+			}
+		}
+	};
+
+	rec(0);
 }
