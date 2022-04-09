d6cced529f7b6df7cf720600e9ac7480a73ca556
Stypox <stypox@pm.me>
2022-04-09 22:19:33 +0200

    Add Kattis peragram: AC

---
+++ more/kattis/peragrams.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cerr<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cerr<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cerr<<", ";}cerr<<*it;}cerr<<"]";return o;}
+void deb(){cerr<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cerr<<t;if(sizeof...(args)!=0){cerr<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+
+signed main() {
+	string s;
+	cin>>s;
+
+	vector<int> lettere(26);
+	for(char c:s){
+		lettere[c-'a']++;
+	}
+
+	int res=0;
+	for(int lettera:lettere){
+		res+=lettera%2;
+	}
+
+	cout<<max(0LL, res-1LL)<<"\n";
+}
