6e458fd8bc3277d8a781c1ff0c78ef1c1f901a9d
Stypox <stypox@pm.me>
2022-04-27 15:18:01 +0200

    Add olinfo task biglietti: 100/100

---
+++ olinfo/biglietti/README.md
@@ -0,0 +1,2 @@
+# [ (biglietti)](https://training.olinfo.it/#/task/biglietti/statement)
+Score: /100
---
+++ olinfo/biglietti/main.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+ifstream in{"input.txt"};
+ofstream out{"output.txt"};
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+
+signed main() {
+	int N,M,A,B;
+	in>>N>>M>>A>>B;
+
+	int v1=N*A;
+	int v2=(N/M)*B+(N%M)*A;
+	int v3=(N/M+1)*B;
+	out<<min({v1,v2,v3})<<endl;
+}
