76e5cfc07b8cef089e811a47ade99d57ef20bf2b
Stypox <stypox@pm.me>
2022-04-27 15:40:24 +0200

    Add olinfo task scrigni: 100/100
    Math probability statistics

---
+++ olinfo/scrigni/README.md
@@ -0,0 +1,2 @@
+# [Super Marco (scrigni)](https://training.olinfo.it/#/task/ois_scrigni/statement)
+Score: 100/100
---
+++ olinfo/scrigni/main.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+ifstream in{"input.txt"};
+ofstream out{"output.txt"};
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+
+signed main() {
+	int N;
+	in>>N;
+	out<<N*(N-1)/4.0L;
+}
