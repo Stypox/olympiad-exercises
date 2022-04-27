29b705de799530d5e5f27cde2936bbc9023f48fd
Stypox <stypox@pm.me>
2022-04-27 17:12:47 +0200

    Add olinfo task truffa: 100/100
    greedy

---
+++ olinfo/truffa/README.md
@@ -0,0 +1,2 @@
+# [Truffa contabile (truffa)](https://training.olinfo.it/#/task/ois_truffa/statement)
+Score: 100/100
---
+++ olinfo/truffa/main.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+ifstream in{"input.txt"};
+ofstream out{"output.txt"};
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+
+signed main() {
+	int N;
+	in>>N;
+	vector<int> V(N);
+	int s=0;
+	for(int n=0;n<N;++n){
+		in>>V[n];
+		s+=V[n];
+	}
+
+	sort(V.begin(), V.end());
+	int res=0;
+	while(s<=0 && res<N){
+		s+=-2*V[res];
+		++res;
+	}
+
+	out<<res;
+}
