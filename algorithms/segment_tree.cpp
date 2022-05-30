3511ac1ab4fcea6097c5c45b86c8ee4ec594334f
Stypox <stypox@pm.me>
2022-05-30 22:43:14 +0200

    Add templated segment tree to algorithms

--- algorithms/segment_tree.cpp
+++ algorithms/segment_tree.cpp
@@ -1,38 +1,48 @@
-struct SegmentTree {
-    static int higherPowerOf2(int x) {
-        int res = 1;
-        while (res < x) res *= 2;
-        return res;
-    }
-
-	vector<int> data;
-	SegmentTree(int n) : data(2 * higherPowerOf2(n), 0) {}
-
-	int query(int i, int a, int b, int x, int y) {
-		if (b <= x || a >= y) return 0;
-		if (b <= y && a >= x) return data[i];
-
-		return query(i*2,   a, (a+b)/2, x, y)
-		     + query(i*2+1, (a+b)/2, b, x, y);
+template<class T>
+class SegmentTree {
+	static int higherPowerOf2(int x) {
+		int res = 1;
+		while (res < x) res *= 2;
+		return res;
 	}
 
-	int update(int i, int a, int b, int x, int v) {
+	const T neutralValue;
+	const T&(*merge)(const T&, const T&);
+	vector<T> data;
+
+	const T& query(int i, int a, int b, int x, int y) {
+		if (b <= x || a >= y) return neutralValue;
+		if (b <= y && a >= x) return data[i];
+
+		return merge(
+			query(i*2,   a, (a+b)/2, x, y),
+			query(i*2+1, (a+b)/2, b, x, y)
+		);
+	}
+
+	const T& update(int i, int a, int b, int x, const T& v) {
 		if (x < a || x >= b) return data[i];
 		if (a == b-1) {
 			assert(a == x);
 			return data[i] = v;
 		}
 
-		return data[i] = update(i*2,   a, (a+b)/2, x, v)
-			            + update(i*2+1, (a+b)/2, b, x, v);
+		return data[i] = merge(
+			update(i*2,   a, (a+b)/2, x, v),
+			update(i*2+1, (a+b)/2, b, x, v)
+		);
 	}
 
-	int query(int x, int y) {
+public:
+	SegmentTree(int n, const T& neutralValue, const T&(*merge)(const T&, const T&)) :
+		neutralValue{neutralValue}, merge{merge}, data(2 * higherPowerOf2(n), neutralValue) {}
+
+	const T& query(int x, int y) {
 		assert(x <= y);
 		return query(1, 0, data.size()/2, x, y);
 	}
 
-	void update(int x, int v) {
+	void update(int x, const T& v) {
 		update(1, 0, data.size()/2, x, v);
 	}
 };
