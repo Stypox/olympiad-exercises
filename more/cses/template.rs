17920dafb84ac400aab28168b486d21d9d5b79d3
Stypox <stypox@pm.me>
2022-10-07 17:04:07 +0200

    Improve Rust template for cses tasks

--- more/cses/template.rs
+++ more/cses/template.rs
@@ -2,8 +2,8 @@ use std::io;
 
 fn main() {
 	let mut input = String::new();
-	io::stdin().read_line(&mut input).expect("expected stdin");
-	let n: i64 = input.trim().parse().expect("expected N");
+	io::stdin().read_line(&mut input).unwrap();
+	let n: i64 = input.trim().parse().unwrap();
 
 	print!("{}\n", n);
 }
