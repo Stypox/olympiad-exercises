7681cd2211a3abe57d633bceb13d751891f95682
Stypox <stypox@pm.me>
2022-11-19 09:51:11 +0100

    Add kattis task areal: AC
    simple

---
+++ kattis/areal.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+#define in cin
+#define out cout
+
+signed main() {
+	int a;
+	in>>a;
+	out<<setprecision(12)<<sqrt((long double)a)*4.0L<<"\n";
+}
