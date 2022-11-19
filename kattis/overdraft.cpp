81439dcb9ddcc77c6e7f881782fb1b3d5ca24fb4
Stypox <stypox@pm.me>
2022-11-19 10:07:44 +0100

    Add kattis task overdraft: AC
    simple

---
+++ kattis/overdraft.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+#define in cin
+#define out cout
+
+signed main() {
+	int N;
+	in>>N;
+	int m=0,s=0;
+	for(int n=0;n<N;++n){
+		int a;
+		in>>a;
+		s+=a;
+		m=min(s,m);
+	}
+	out<<-m<<"\n";
+}
