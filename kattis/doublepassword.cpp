3876860af0440466c6c43d747ed1b61b042a52f1
Stypox <stypox@pm.me>
2022-11-19 10:02:01 +0100

    Add kattis task doublepassword: AC
    simple string

---
+++ kattis/doublepassword.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define float long double
+
+#ifdef DEBUG
+template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
+void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
+#else
+#define deb(...)
+#endif
+#define in cin
+#define out cout
+
+signed main() {
+	string s1,s2;
+	in>>s1>>s2;
+	int ceq=0;
+	for(int i=0;i<4;++i){
+		if(s1[i]!=s2[i]) ceq+=1;
+	}
+	out<<(1<<ceq)<<"\n";
+}
