e166013320111fae80d3c3d7c810903dac1002d3
Stypox <stypox@pm.me>
2023-05-22 17:00:25 +0200

    Change montresor task got, but even worse

--- more/montresor/got/got.cpp
+++ more/montresor/got/got.cpp
@@ -1,19 +1,20 @@
 #pragma GCC optimize ("O3")
 #include <bits/stdc++.h>
-#include "got.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 using namespace std;
 
 ifstream in{"input.txt"};
 ofstream out{"output.txt"};
 #ifdef DEBUG
 template<class A,class B>ostream&operator<<(ostream&o,const pair<A,B>&p){cout<<"("<<p.first<<", "<<p.second<<")";return o;}
-template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";if(distance(v.begin(), it) % 5 == 0)cout<<"\n ";}cout<<setw(2)<<*it;}cout<<"]";return o;}
+template<class T,typename=typename enable_if<!is_same<T, string>::value,decltype(*begin(declval<T>()))>::type>ostream&operator<<(ostream&o,const T&v){cout<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin()){cout<<", ";}cout<<*it;}cout<<"]";return o;}
 void deb(){cout<<"\n";}template<class T,class...Ts>void deb(const T&t,const Ts&...args){cout<<t;if(sizeof...(args)!=0){cout<<"  ";}deb(args...);}
 #else
 #define deb(...)
 #endif
-#define cout Do not use cout!!!
-#define cin Do not use cin!!!
 
 using sint = uint16_t; // small int
 using fint = uint_fast16_t; // fast int
@@ -88,7 +89,34 @@ struct Sol {
     }
 };
 
+vector<Sol> sols;
+void setupTimeLimit(){
+    struct sigaction new_action;
+    new_action.sa_handler = [](int signum) {
+        int besti, bestt=0;
+        for(int i=0;i<sols.size();++i){
+            if(sols[i].taken>bestt){
+                bestt=sols[i].taken;
+                besti=i;
+            }
+        }
+        sols[besti].print();
+        quick_exit(0);
+    };
+    sigemptyset(&new_action.sa_mask);
+    sigaddset(&new_action.sa_mask, SIGXCPU);
+    new_action.sa_flags = 0;
+    sigaction(SIGXCPU,&new_action,NULL);
+
+    struct rlimit limit;
+    getrlimit(RLIMIT_CPU, &limit);
+    limit.rlim_cur = 2;
+    setrlimit(RLIMIT_CPU, &limit);
+}
+
 signed main() {
+    setupTimeLimit();
+
     in>>R>>C;
     grid.resize(R*C);
     for(fint i=0;i<R*C;++i){
@@ -98,9 +126,10 @@ signed main() {
         }
     }
     sort(castles.begin(), castles.end());
+    deb("Castles:",castles.size());
 
-    for(int i=0;i<R*C;++i){
-        for(int j=0;j<R*C;++j){
+    /*for(fint i=0;i<R*C;++i){
+        for(fint j=0;j<R*C;++j){
             if(manh_dist(i,j) != manh_dist(j,i)){
                 deb("Different", i, j, manh_dist(i,j), manh_dist(j,i));
                 assert(false);
@@ -115,161 +144,88 @@ signed main() {
                 assert(false);
             }
         }
-    }
+    }*/
 
-    vector<Sol> sols(min((size_t)1024*1024/R/C, (size_t)1000));
-    fint bestTaken=sols[0].taken;
-    auto onFoundSol = [&](Sol& newSol){
-        if(newSol.taken>bestTaken){
-            newSol.print();
-            bestTaken=newSol.taken;
-        }
-        for(int j=0;j<5;++j){
-            fint si=rand()%sols.size();
-            if(newSol.taken >= sols[si].taken){
-                sols[si] = newSol;
-            }
-        }
-    };
-
+    sols.resize(2);
     vector<sint> used(R*C,0);
-    vector<fbint> points(R*C);
+    vector<sint> buf(4*R*C+1);
+    size_t bufb=0,bufe=1;
     sint usedi=1;
 
+    int64_t iter=0;
     while(true){
         fint si=rand()%sols.size();
-        const Sol& sol = sols[si];
-        const auto& mem = sol.mem;
+        Sol& sol = sols[si];
+        auto& mem = sol.mem;
 
-        fint ci=rand()%min((int)sol.usableCastles.size(),max((int)castles.size()/10,(int)20));
+        fint ci=rand()%sol.usableCastles.size();
         auto cit=sol.usableCastles.begin();
         advance(cit, ci); // TODO optimizable
         const Castle& cas = castles[*cit];
+        for_neighbors(cas.i, neigh, {
+            if (mem[neigh]!=0 && mem[neigh]!=cas.size){
+                // this castle can't ever be taken since it is near a zone with the same wanted size
+                sol.usableCastles.erase(cit);
+                continue;
+            }
+        })
 
         fint touchedCastlesSoFar=0;
         fint oneTouchedCastle=0; // chooses one random castle among touched ones
-        vector<fint> q;
-        q.push_back(cas.i);
-        used[cas.i]=usedi;
-        points[cas.i]=0;
+        fint usedCount=0;
 
-        for(int qi=0; qi!=q.size(); ++qi){
-            fint i=q[qi];
-            fint dist = manh_dist(i, cas.i);
+        bufb=0; bufe=1; buf[0]=cas.i;
+        while(bufb!=bufe){
+            sint i = buf[bufb++];
 
-            for_neighbors(i, neigh, {
-                points[neigh]=0;
-                if (mem[neigh]!=0){
-                    if (rand()%(touchedCastlesSoFar+1)==0){
-                        oneTouchedCastle=neigh;
-                        touchedCastlesSoFar+=1;
-                    }
-                } else if (dist<cas.size-1 && (grid[neigh]==0||grid[neigh]==cas.size) && used[neigh]!=usedi) {
-                    bool foundbadneighbor=false;
-                    for_neighbors(neigh, neigh2, {
-                        if(mem[neigh2]==cas.size){
-                            foundbadneighbor = true;
-                        }
-                    })
-                    if (!foundbadneighbor) {
-                        used[neigh]=usedi;
-                        q.push_back(neigh);
-                    }
-                }
-            })
+            if (used[i] == usedi) continue;
+            for_neighbors(i, neigh, if(mem[neigh] == cas.size) continue;)
+            if (mem[i] != 0 || !(grid[i]==0 || grid[i]==cas.size) || manh_dist(i, cas.i) >= cas.size) continue;
+
+            used[i] = usedi;
+            usedCount += 1;
+            for_neighbors(i, neigh, buf[bufe++] = neigh;)
         }
 
-        for(int qi=q.size()-1; qi>=0; --qi){
-            fint i=q[qi];
-            for_neighbors(i, neigh, {
-                if(mem[neigh]==cas.size){
-                    continue;
-                }
-            })
-            fint dist = manh_dist(i, cas.i);
-
-            int p=1;
-            if(grid[i]==cas.size){
-                p+=2;
-            }
-            int neighCount=0;
-            int neighSum=0;
-            for_neighbors(i, neigh, {
-                if (manh_dist(neigh, cas.i) > dist) {
-                    if (mem[neigh]!=0){
-                        p+=1;
-                    } else if(points[neigh]!=0) {
-                        neighCount+=1;
-                        neighSum+=points[neigh];
-                    }
-                }
-            })
-            if(neighCount!=0){
-                p+=(neighSum+neighCount-1)/neighCount;
-            }
-            points[i]=p;
-            if (points[i]==0){
-                deb("Lo sto mettendo a zero");
-            }
-        }
-
-        if(points[cas.i]!=0 && q.size()>=cas.size){
-            Sol newSolFill = sol;
-            priority_queue<pair<fbint,fint>> pq;
-            pq.push(pair<fbint,fint>(0,cas.i));
+        //deb(usedCount, cas.size);
+        if (usedCount>=cas.size) {
+            bufb=0; bufe=1; buf[0]=cas.i;
             int toFill=cas.size;
-
-            auto prevPoints=points;
             while(toFill>0){
-                if(pq.empty()){
-                    sol.debprint();
-                    newSolFill.debprint();
-                    deb(points);
-                    deb(prevPoints);
-                    deb(used);
-                    deb(usedi,cas.i,cas.size,q.size(),q);
-                    deb(manh_dist(43,44), manh_dist(35,31), manh_dist(35,41), manh_dist(44,43), manh_dist(31,35), manh_dist(41,35));
-                    assert(false);
-                }
-                fint i=pq.top().second;
-                pq.pop();
-                if(points[i]==0) continue;
-                toFill--;
+                sint i = buf[bufb++];
 
-                assert(newSolFill.mem[i]==0);
-                assert(grid[i]==0 || grid[i]==cas.size);
-                newSolFill.mem[i]=cas.size;
-                for_neighbors(i, neigh, {
-                    if (points[neigh]!=0){
-                        pq.push(pair<fbint,fint>(points[neigh],neigh));
-                    }
-                })
-                points[i]=0;
+                if (used[i] != usedi) continue;
+                used[i] = 0;
+                mem[i] = cas.size;
+
+                for_neighbors(i, neigh, buf[bufe++] = neigh;)
+                if(rand() < INT_MAX/min(toFill,10)){
+                    int a=rand()%(bufe-bufb);
+                    int b=rand()%(bufe-bufb);
+                    swap(buf[bufb+a], buf[bufb+b]);
+                }
+
+                toFill--;
             }
 
-            newSolFill.taken += cas.size;
-            newSolFill.usableCastles.erase(cas.i);
-            onFoundSol(newSolFill);
-            //newSolFill.debprint();
-
-        }else if(touchedCastlesSoFar!=0&&q.size()+sol.mem[oneTouchedCastle]>=cas.size){
-
+            sol.taken += cas.size;
+            sol.usableCastles.erase(cas.i);
         }
 
         usedi+=1; // let it overflow
-        if(usedi==0 ||
-            #ifdef DEBUG
-                true
-            #else
-                false
-            #endif
-        ){
+        if(usedi==0){
             usedi+=1;
             fill(used.begin(),used.end(),0);
         }
 
-        //if(cas.size == 9)break;
+        iter+=1;
+        if (iter % ((R+C)*10) == 0 && sols.size() <= min((R+C)/2, 1024*1024/(R*C+R+C+128))) {
+            int ssize=sols.size();
+            for(int s=0;s<ssize;++s){
+                sols.push_back(sols[s]);
+            }
+            deb("Iter:", iter, "Score:", sols[0].taken);
+        }
     }
-
     return 0;
 }
